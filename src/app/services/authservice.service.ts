a8f7acc38efcef29ed588a4479f8fbb069ab0fe2
dilip <dilip.pakharee@gmail.com>
2022-02-15 13:51:55 +0530

    tt

--- src/app/services/authservice.service.ts
+++ src/app/services/authservice.service.ts
@@ -13,12 +13,23 @@ export class AuthserviceService {
    }
 
   // readonly BaseURL='https://servicew.wheebox.com/WheeboxRestService_blob/'
-  readonly BaseURL='https://servicew.wheebox.com/WheeboxRestService_blob/'
+ // readonly BaseURL='https://servicew.wheebox.com/WheeboxRestService_blob/'
+
+ readonly BaseURL='https://localhost:44317/api/'
    SignUp(data:any){
      debugger;
      let AuthHeader=new HttpHeaders();
      AuthHeader.append('Access-Control-Allow-Origin','*');
     return this.http.post("https://localhost:44317/api/UserToken",data,{headers:AuthHeader});
+  }
+
+ 
+   postApi(data:any,method:any){
+     debugger;
+     let AuthHeader=new HttpHeaders();
+     AuthHeader.append('Access-Control-Allow-Origin','*');
+     AuthHeader.append('Authrization',this.token);
+    return this.http.post(this.BaseURL+"/"+method,data,{headers:AuthHeader});
   }
 
 }
