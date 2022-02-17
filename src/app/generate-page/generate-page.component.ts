3ec891dca3846f759b5716937161adc903083749
Sujata <sujata.parvate@neosoftmail.com>
2022-02-17 13:14:04 +0530

    generate page

--- src/app/generate-page/generate-page.component.ts
+++ src/app/generate-page/generate-page.component.ts
@@ -1,6 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthserviceService } from '../services/authservice.service';
 
+export interface PageTypes {
+  value: string;
+  display: string;
+}
+export interface Captureimages {
+  value: string;
+  display: string;
+}
 
 @Component({
   selector: 'app-generate-page',
@@ -8,12 +16,26 @@ import { AuthserviceService } from '../services/authservice.service';
   styleUrls: ['./generate-page.component.css']
 })
 export class GeneratePageComponent implements OnInit {
-
+ 
+  PageTypes: PageTypes[] = [
+    {value: 'env', display: 'env'},
+    {value: 'approver', display:'approver'},
+    {value: 'env-approver', display:'env-approver'},
+    {value: 'train', display:'train'}
+ ];
+ Captureimages: Captureimages[] = [
+  {value: 'candidate', display: 'candidate'},
+  {value: 'idcard', display:'idcard'},
+  {value: 'both', display:'both'}
+ 
+];
   constructor(public authoservice:AuthserviceService) { }
 
   ngOnInit(): void {
   }
   str : string='';
+
+  
   GetURL(data:any)
   {
     debugger;
